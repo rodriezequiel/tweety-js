7e86f80b6befc04c86535b78468d56b955bd60d8
belenbuccolo <belenbuccolo@gmail.com>
2021-05-13 16:06:42 -0300

    hola

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-
-const express = require( 'express' );
+const express = require('express');
 const morgan = require('morgan'); //middleware application logger
-const nunjucks = require( 'nunjucks' );
+const nunjucks = require('nunjucks');
 
 const app = express(); // crea una instancia de una aplicación de express
 
@@ -11,27 +10,23 @@ app.engine('html', nunjucks.render); // cuando le den archivos html a res.render
 nunjucks.configure('views'); // apunta a nunjucks al directorio correcto para los templates
 
 app.use(morgan('tiny'));
-app.use(express.static('./public'))
+app.use(express.static('./public'));
 
 let tweetsDeEjemplo = [
-    { id: 1, name: "juan", content: "este es un tweeettt de juan" },
-    { id: 2, name: "carlos", content: "este es un tweeettt de carlos" },
-    { id: 3, name: "pepe", content: "este es un tweeettt de pepe" },
+    { id: 1, name: 'juan', content: 'este es un tweeettt de juan' },
+    { id: 2, name: 'carlos', content: 'este es un tweeettt de carlos' },
+    { id: 3, name: 'pepe', content: 'este es un tweeettt de pepe' },
 ];
 
 app.get('/', function (req, res) {
-    res.render( 'index', { tweets: tweetsDeEjemplo });
-
+    res.render('index', { tweets: tweetsDeEjemplo });
 });
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log('Servidor corriendo en http://localhost:3000/');
 });
 
-
-
-
-
+// hola
 
 // app.get('/stylesheets/style.css', (req,res) =>{
 //     res.sendFile(__dirname+"/public/stylesheets/style.css");
@@ -45,10 +40,8 @@ app.listen(3000, function(){
 
 // function logIn (req, res, next) {
 //     // hacé tu logueo acá, probá console.log(req)
-//     // llamá a `next()`. Sino tu app recibirá pedidos 
+//     // llamá a `next()`. Sino tu app recibirá pedidos
 //     // pero no responderá adecuadamente.
 //  console.log(`${req.method} ${req.url}`);
 //     next();
 // }
-
-
